dfd7e5f03be7da60e8682256eb590f039f96cf10
Alexa <alex_krim99@mail.ru>
2023-03-27 22:42:38 +0300

    Выполнила 3 задачи

---
+++ task_34/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int[] array = new int[8];
+int count = 0;
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(FindChetnie(count, array));
+
+
+
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i  < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+int FindChetnie(int count, int[] array)
+{
+    foreach(int i in array)
+        {
+            if(i%2 == 0)
+            {
+                count++;
+             }
+        }
+    return count;
+}
---
+++ task_36/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int[6];
+int sum = 0;
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"Сумма элементов с нечетными индексами равна {SumElementovSNechIndex(sum, array)}, индексация начинается с 0.");
+
+
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i  < array.Length; i++)
+        array[i] = new Random().Next(-100, 100);
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+
+int SumElementovSNechIndex(int sum, int[] array)
+{
+    for (int index = 0; index < array.Length; index++)
+        {
+            int i = array[index];
+            if (index %2 != 0) 
+            {
+                sum = sum + i;
+            }
+        }
+    return sum;
+}
+
